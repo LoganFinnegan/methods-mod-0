1424c99ef6d87567f414851b5d857c0b6c1abe4e
Logan Finnegan <logantfinnegan@protonmail.com>
2023-09-30 19:01:46 -0500

    finished methods

--- final_practice.rb
+++ final_practice.rb
@@ -5,31 +5,53 @@
 
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
+def greeting
+    return "hello friend"
+end
 
 # What is the return value of your method?
+#hello friend
 # How many arguments did you pass your method?
+#0 
 
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
+def custom_greeting(Logan)
+    return "Hello #{Logan}"
+end
 
 # What is the return value of your method?
+#Hello Logan
 # How many arguments did you pass your method?
+#1
 # What data type was your argument(s)?
+#string
 
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
+def greet_person(Logan)+(Thomas)+(Finnegan)
+    return "Hello #{Logan} + #{Thomas} + #{Finnegan}"
+end
 # What is the return value of your method?
+#Hello Logan Thomas Finnegan
 # How many arguments did you pass your method?
+#3
 # What data type was your argument(s)?
-
+#string
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
+numbers = [1,2,3,4,5]
+def numbers[1].square!
+    puts numbers[1].square!
+end
 
 # What is the return value of your method?
+#1
 # How many arguments did you pass your method?
+#1
 # What data type was your argument(s)?
+# integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
@@ -37,6 +59,13 @@
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
+
+def 
+    if check_stock > 0
+        puts "Coffe is stocked"
+    else 
+        puts "Coffee is stocked"
+    end
 
 check_stock(3, "Tortillas");
 # => "Tortillas - running LOW"
--- self_eval.md
+++ self_eval.md
@@ -1,4 +1,6 @@
 # Self-Evaluation
 
 - Reflect on your learning and work today. What are you most proud of?
+# it was a bit of a struggle. I'm not at home and I was having a hard time without my second monitor trying to look things up and my family situation is distracting. But I kept grinding on it and reached out when I got stuck so I was able to grind through things. 
 - What is the power of using methods in our applications?
+# it allows us to set up systems that will trigger automatically/ update as the imputs do. this is essential for any larger system/opperation that needs to handle significat data as well as making routin tasks automatic. 
